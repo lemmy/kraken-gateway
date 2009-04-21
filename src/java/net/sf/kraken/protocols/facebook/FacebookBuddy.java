2d0c10e47b4f19ffbab7f0c6400be6488634c950
Daniel Henninger <daniel@vorpalcloud.org>
2009-04-21 04:16:45 +0000

    Applied Patrick Li's patch to add avatar support to Facebook!

--- src/java/net/sf/kraken/protocols/facebook/FacebookBuddy.java
+++ src/java/net/sf/kraken/protocols/facebook/FacebookBuddy.java
@@ -10,13 +10,19 @@
 
 package net.sf.kraken.protocols.facebook;
 
+import net.sf.kraken.avatars.Avatar;
 import net.sf.kraken.roster.TransportBuddy;
 import net.sf.kraken.roster.TransportBuddyManager;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
 import java.util.Arrays;
 
 /**
  * @author Daniel Henninger
+ * @author Patrick Li
  */
 public class FacebookBuddy extends TransportBuddy {
     
@@ -25,10 +31,41 @@ public class FacebookBuddy extends TransportBuddy {
     public FacebookBuddy(TransportBuddyManager manager, FacebookUser user) {
         super(manager, user.uid, user.name, Arrays.asList("Facebook"));
         this.user = user;
+        
+        // We also want to initialize their avatar picture here
+        Avatar avatar = new Avatar(getJID(), user.thumbSrc, readUrlBytes(user.thumbSrc));
+        setAvatar(avatar);
     }
     
     public FacebookUser getUser() {
         return user;
     }
 
+    public byte[] readUrlBytes(String address) {
+        byte[] byteReturn = null;
+        URL theURL = null;
+        InputStream inStream = null;
+        try {
+            theURL = new URL(address);
+            inStream = theURL.openStream();
+            ByteArrayOutputStream os = new ByteArrayOutputStream();
+            int c;
+            while((c = inStream.read()) != -1) {
+              os.write(c);
+            }
+            byteReturn = os.toByteArray();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (inStream != null) {
+                  inStream.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return byteReturn;
+    }
+    
 }
